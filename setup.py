072630272b986373f7f18fe451c894a340916781
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-05-07 16:35:27 +0530

    Changing name again

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     license='MIT',
     author="Maran Sowthri Kalailingam",
     author_email='contact@maransowthri.dev',
-    packages=find_packages('nameing_utils'),
-    package_dir={'': 'nameing_utils'},
+    packages=find_packages('naming_utils'),
+    package_dir={'': 'naming_utils'},
     url='https://github.com/maransowthri/naming-utils.git',
     keywords='naming utils'
 )
