8e579d796e57813718c3d561129853198d20d51b
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-05-07 16:20:28 +0530

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='file_utils',
+    version='0.1',
+    license='MIT',
+    author="Maran Sowthri Kalailingam",
+    author_email='contact@maransowthri.dev',
+    packages=find_packages('file_utils'),
+    package_dir={'': 'file_utils'},
+    url='https://github.com/maransowthri/file-utils.git',
+    keywords='file utils'
+)
