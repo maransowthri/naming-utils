492351515530ec448c79fd8ace4f63f14bbe0f63
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-05-08 10:08:57 +0530

    Releasing first package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='naming_utils',
-    version='0.4',
+    version='0.5',
     packages=find_packages('src'),
     package_dir={'': 'src'},
 )
