8cff9249502baef203ad7e94361cffbdf0176a5f
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-05-08 09:24:54 +0530

    Changing folder structures

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='naming_utils',
-    version='0.1',
+    version='0.3',
     license='MIT',
     author="Maran Sowthri Kalailingam",
     author_email='contact@maransowthri.dev',
-    packages=find_packages('naming_utils'),
-    package_dir={'': 'naming_utils'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     url='https://github.com/maransowthri/naming-utils.git',
     keywords='naming utils'
 )
--- tests/naming_utils/test_naming_utils.py
+++ tests/naming_utils/test_naming_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from naming_utils import unique_name_with_ts, unique_names_with_ts
+from src.naming_utils import unique_name_with_ts, unique_names_with_ts
 
 
 class TestNamingUtils(unittest.TestCase):
