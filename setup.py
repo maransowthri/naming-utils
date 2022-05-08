953c60b8b7870effa800295469c7509dfc84cb0c
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-05-08 09:55:35 +0530

    Adding more details to setup.cfg

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='naming_utils',
     version='0.1',
-    license='MIT',
-    author="Maran Sowthri Kalailingam",
-    author_email='contact@maransowthri.dev',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/maransowthri/naming-utils.git',
 )
